56fd9521720c4aad1c097bbc45f3e0573c21f707
Aditya Kaushal <adityakaushal716@gmail.com>
2025-09-29 13:14:17 +0530

    Fix: Ensure infoBox.css is present and imported correctly

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -3,7 +3,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
-import './infoBox.css'
+// import './infoBox.css'
 
 export default function InfoBox({ info }) {
     // const photo="https://plus.unsplash.com/premium_photo-1729600377083-bbe558d8b7a6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
